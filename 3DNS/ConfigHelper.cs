c6f21aa763dd779f09572359d0a13d8eb645142e
Tom Oddy <tom_oddy@live.co.uk>
2026-03-28 21:37:12 +0000

    Added config loading

---
+++ 3DNS/ConfigHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Configuration;
+
+namespace _3DNS
+{
+    /// <summary>
+    /// Config helper methods
+    /// </summary>
+    internal static class ConfigHelper
+    {
+        /// <summary>
+        /// Gets value from configuration
+        /// </summary>
+        /// <param name="logger">Logger</param>
+        /// <param name="key">Key</param>
+        /// <returns>Config value</returns>
+        /// <exception cref="KeyNotFoundException">Thrown if config value not present</exception>
+        public static string GetValue(ILogger logger, string key)
+        {
+            string? value = ConfigurationManager.AppSettings[key];
+            if (value is null)
+            {
+                logger.LogError("{key} is not set in the configuration.", key);
+                throw new KeyNotFoundException($"{key} is not set in the configuration.");
+            }
+
+            logger.LogInformation("{key} loaded successfully ({value}).", key, value);
+            return value;
+        }
+    }
+}
--- 3DNS/Program.cs
+++ 3DNS/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _3DNS;
+using Microsoft.Extensions.Logging;
+
+/// <summary>
+/// Entry point
+/// </summary>
+internal class Program
+{
+    /// <summary>
+    /// Main method
+    /// </summary>
+    private static void Main()
+    {
+        // Create logger
+        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+        ILogger logger = factory.CreateLogger("3DNS");
+
+        // Load configuration
+        string domain = ConfigHelper.GetValue(logger, "Domain");
+        string apiKey = ConfigHelper.GetValue(logger, "ApiKey");
+        string apiSecret = ConfigHelper.GetValue(logger, "ApiSecret");
+    }
+}
